c670a308fd87ff847f39a52fa4f2dea8014d55f2
njnovo <75146085+njnovo@users.noreply.github.com>
2025-02-10 23:52:16 -0700

    ...

--- src/components/calendar-hero.tsx
+++ src/components/calendar-hero.tsx
@@ -3,15 +3,7 @@ import Image from "next/image";
 export function CalendarHero() {
   return (
     <div className="relative min-h-screen w-full bg-blue-900">
-      <div className="container relative mx-auto flex min-h-screen items-center">
-        {/* Left circular image*/}
-        <div className="absolute left-0 h-[400px] w-[400px] overflow-hidden rounded-full">
-          <Image
-            src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dreamstime.com%2Fphotos-images%2Fgroup-little-students.html%3Fpg%3D8%26view%3Dlatest-uploads&psig=AOvVaw1geZwET4e4R9uQUlo_tO_v&ust=1736830145151000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIDI8tLy8YoDFQAAAAAdAAAAABAh"
-            alt="People Who Go to School"
-            className="h-full w-full object-cover"
-          />
-        </div>
+      <div className="container relative w-full flex min-h-screen items-center">
 
         {/* Content - centered on mobile, offset on desktop */}
         <div className="ml-[300px] w-full max-w-2xl text-white lg:ml-[400px]">
