0cc15cf5f498a04544be05c26adedcddb5b4ebbf
njnovo <75146085+njnovo@users.noreply.github.com>
2025-02-10 23:56:12 -0700

    layout

--- src/app/(calendar)/layout.tsx
+++ src/app/(calendar)/layout.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col bg-background">
+    <div className="w-full bg-background">
       <Header white />
       {children}
     </div>
