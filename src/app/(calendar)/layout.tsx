4ca400a5e9c23a9b5078dfbe4c1e7d641a76070e
njnovo <75146085+njnovo@users.noreply.github.com>
2025-02-10 22:35:11 -0700

    layout fix attempt 2

--- src/app/(calendar)/layout.tsx
+++ src/app/(calendar)/layout.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex min-h-screen flex-col overflow-x-hidden bg-background">
       <Header white />
       {children}
     </div>
